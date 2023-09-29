fab6e5eb3394c0559d09a11d8dca6fadadd078e1
Santiago <sanchezcsantiago@gmail.com>
2023-09-28 21:01:32 -0600

    ProxyBot

---
+++ src/main/java/resources/ProxyResource.java
@@ -0,0 +1,32 @@
+package resources;
+
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+@Path("/proxy")
+public class ProxyResource {
+
+    @GET
+    public Response proxyRequest(@QueryParam("url") String url) {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet httpGet = new HttpGet(url);
+
+            CloseableHttpResponse response = httpClient.execute(httpGet);
+
+
+            String responseBody = EntityUtils.toString(response.getEntity());
+
+            return Response.ok(responseBody).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(500).entity("Internal Server Error").build();
+        }
+    }
+}
